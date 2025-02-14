5a0b0e61457b586bd1a039c4373bb5f364eec393
ayoubElhoucine <ayoubelhoucine@gmail.com>
2025-02-14 18:18:20 +0100

    update swift package

--- Package.swift
+++ Package.swift
@@ -19,11 +19,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "shared",
-            path: "shared.xcframework"
+            url: "https://github.com/ayoubElhoucine/Test-KMP-Library2/releases/download/1.0.3/shared.xcframework.zip",
+            checksum: "340dea794567e21024f17751865f21e74d44353bc9dc3ded7cd9438175a5ba3f"
         ),
         .binaryTarget(
             name: "submodule",
-            path: "submodule.xcframework"
+            url: "https://github.com/ayoubElhoucine/Test-KMP-Library2/releases/download/1.0.3/submodule.xcframework.zip",
+            checksum: "d2a65dcc26849a6c9a9545e3fe44a376e2d0c75118f4268748aba729be1a0891"
         )
     ]
 )
